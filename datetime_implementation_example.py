dfd2ffff2c43727be8e4a2f6f074e5384db489bc
nicholashegarty <nicholasahegartynah@gmail.com>
2023-02-23 11:39:44 +0000

    Removed notebook, split into 3 py files, and loopable

---
+++ datetime_implementation_example.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+now = datetime.now()#datetime
+print(now)
+
+timestamp = datetime.timestamp(now) #int
+print(timestamp)
+
+dt_object= datetime.fromtimestamp(timestamp) #object
+print(type(dt_object))
+print(dt_object)
