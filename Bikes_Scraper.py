386ff0856ada46b8dac8acaa864dbaef54a52112
bomahony1 <bomahony1999@gmail.com>
2023-03-03 13:37:45 +0000

    Updated log in details

--- Bikes_Scraper.py
+++ Bikes_Scraper.py
@@ -1,63 +1,52 @@
-import sqlalchemy 
 from sqlalchemy import create_engine
 import traceback
-import glob
-import os
-from pprint import pprint
 import simplejson as json
 import requests
 from datetime import datetime
 import time
-from IPython.display import display
-URI = "dbbikes2.cytgvbje9wgu.us-east-1.rds.amazonaws.com"
+
+
+URI = "demodublinbikes3.cix4fu1foszu.eu-west-1.rds.amazonaws.com"
 PORT = "3306"
-DB = "dbbikes2"
+DB = "demodublinbikes3"
 USER = "admin"
-PASSWORD = "DublinBikes1"
+PASSWORD = "feed123456"
+
 engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+
 JCKEY = "8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
 NAME = "Dublin"
 URI =  "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
 
-#r = requests.get(URI, params= {"api_key":JCKEY, "contract": NAME})
-#json.loads(r.text)
-
+##Read availability json data and insert dynamic values to db
 def availability_to_db(text):
     stations = json.loads(text)
     for station in stations:
-        #print(station)
-        vals = (station.get('number'), station.get('available_bikes'), station.get('available_bike_stands'), datetime.timestamp(datetime.now()))
-        engine.connect().execute("insert into availability values(%s,%s,%s,%s)", vals)
-        
+        vals = (station.get('number'), station.get('available_bikes'), station.get('available_bike_stands'), station.get('status'), datetime.timestamp(datetime.now()))
+        engine.connect().execute("insert into availability2 values(%s,%s,%s,%s,%s)", vals)
     return
 
+##Read weather json data and insert dynamic values to db
 def weather_to_db(text):
     vals = (text["weather"][0]["main"], text["weather"][0]["description"], text["main"]["temp"], text["visibility"], text["wind"]["speed"], text["wind"]["deg"], text["clouds"]["all"], datetime.timestamp(datetime.now()))
-    engine.connect().execute("insert into weather values(%s,%s,%s,%s,%s,%s,%s,%s)", vals)
+    engine.connect().execute("insert into weather2 values(%s,%s,%s,%s,%s,%s,%s,%s)", vals)
 
     return
-    
 
-# Weather scraper
-API_KEY = "d5de0b0a9c3cc6473da7d0005b3798ac"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_name = "Dublin, IE"
-complete_url = base_url + "appid=" + API_KEY + "&q=" + city_name
-response = requests.get(complete_url)
-# Save json data into a variable called x
-weather = response.json()
+##URL's with parameters already included
+URI1 =  "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
+URI2 = "http://api.openweathermap.org/data/2.5/weather?appid=d5de0b0a9c3cc6473da7d0005b3798ac&q=Dublin, IE"
 
-
+##loop every 5 mins to call both api requests, and call update table funcs
 def main():
     while True:
         try:
-            r = requests.get(URI, params= {"api_key":JCKEY, "contract": NAME})
-            availability_to_db(r.text)
-            weather_to_db(weather)
+            r1 = requests.get(URI1)
+            availability_to_db(r1.text)
+            r2 = requests.get(URI2)
+            weather_to_db(r2.json())
             time.sleep(5*60)
         except:
-            print(traceback.format_tb.execute())
-            #if engine is None:
+            print("Error, no longer running")
     return
-
 main()
