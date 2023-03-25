050e35734d0102e020de3458274b7c1b8ba2b096
bomahony1 <bomahony1999@gmail.com>
2023-03-25 16:07:29 +0000

    Added Info Window Feature

--- static/index.js
+++ static/index.js
@@ -1,39 +1,66 @@
 let map;
+
+// Initialize and add the map
 function initMap() {
   const dublin = { lat: 53.350140, lng: -6.266155 }
   map = new google.maps.Map(document.getElementById("map"), {
     center: dublin,
     zoom: 14,
   });
-  // The marker, positioned at Dublin
-  // const marker = new google.maps.Marker({
-  //   position: dublin,
-  //   map: map,
-  // })
+
   getStations();
 }
 
 window.initMap = initMap;
 
-function addMarkers(stations) {
+
+// fetch stations
+function getStations() {
+  fetch("/stations") 
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('fetch response', typeof data);
+      addMarkersAndInfoWindow(data);
+    });
+}
+
+// ***** CODE FOR ADDING MARKERS *****
+
+
+function addMarkersAndInfoWindow(stations) {
   for (const station of stations) {
+
+    // Place markers on station locations
+
     var marker = new google.maps.Marker({
       position: {
         lat: station.position_lat,
         lng: station.position_lng,
       }, 
       map: map,
-      title: station.name,
+      title: station.address,
       station_number: station.number,
+    });   
+
+    //  ***** CODE FOR INFO-WINDOW *****
+
+    const contentString = "<div><h1>" + marker.title + "</h1></div>";
+
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      ariaLabel: "Uluru",
     });
+
+    google.maps.event.addListener(marker, 'click', function(marker) {
+      return function() {
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
+      }
+    }(marker));
   }
+
+  
 }
 
-function getStations() {
-  fetch("/stations") 
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('fetch response', typeof data);
-      addMarkers(data);
-    });
-}
+
+
