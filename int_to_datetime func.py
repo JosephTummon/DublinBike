4cdb22d26741da6ea05feb4be3bca049feb9abd1
Joseph Tummon <96840768+JosephTummon@users.noreply.github.com>
2023-03-30 15:22:03 +0100

    Delete int_to_datetime func.py

--- int_to_datetime func.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-#represents integer stored in database
-a = datetime.timestamp(datetime.now())
-print(a)
-
-#converts database integer to datetime format
-b = datetime.fromtimestamp(a)
-print(b)
