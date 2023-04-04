b8ae69ec34b7570dddbf1339d3b66fafd3c78570
bomahony1 <bomahony1999@gmail.com>
2023-04-04 20:49:49 +0100

    Added function to scrape JCDeaux API for Bike availability data

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, g, render_template, jsonify
 from sqlalchemy import create_engine, text
 import traceback
 import functools
-import pandas as pd
+import requests
+import time
+import simplejson as json
+from datetime import datetime
 
 URI = "dbbikes2.cytgvbje9wgu.us-east-1.rds.amazonaws.com"
 PORT = "3306"
@@ -11,8 +14,7 @@ USER = "admin"
 PASSWORD = "DublinBikes1"
 engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-app = Flask("__name__", template_folder="templates")
-app._static_folder = "static"
+app = Flask("__name__")
 
 def connect_to_database():
     engine = create_engine("mysql://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB), echo=True)
@@ -52,43 +54,82 @@ def home():
     co_ordinates = load_station_coordinates()
     return render_template('index.html')
 
+# @app.route("/stations")
+# @functools.lru_cache(maxsize=128)
+# def get_stations():
+#     engine = connect_to_database()
+#     sql = f"""SELECT s.address, 
+#                 s.position_lat, 
+#                 s.position_lng, 
+#                 a.available_bike_stands as bike_stands,
+#                 a.available_bikes as bikes_free,
+#                 a.datetime
+#             FROM dbbikes2.availability2 as a
+#             JOIN dbbikes2.station as s
+#             ON s.number = a.number
+#             JOIN (
+#                 SELECT number, MAX(datetime) as max_datetime
+#                 FROM dbbikes2.availability2
+#                 GROUP BY number
+#             ) as max_times
+#             ON a.number = max_times.number AND a.datetime = max_times.max_datetime
+#             WHERE s.number = a.number
+#             ORDER BY s.address;"""
+#     try:
+#         with engine.connect() as conn:
+#             rows = conn.execute(text(sql)).fetchall()
+#             print('#found {} stations', len(rows), rows)
+#             return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
+#     except:
+#         print(traceback.format_exc())
+#         return "error in get_stations", 404
+
+# Define the main function that will call the get_stations function every 30 seconds
+def update_data():
+    while True:
+        try:
+            # Call the get_stations function and update the stations variable
+            stations = get_stations()
+            print("Data updated at {}".format(datetime.now()))
+
+            # Sleep for 30 seconds before calling the function again
+            time.sleep(30)
+        except Exception as e:
+            print(traceback.format_exc())
+            print("Error updating data: {}".format(e))
+            # Sleep for 30 seconds before calling the function again
+            time.sleep(30)
+
+# Define the route for /stations
 @app.route("/stations")
 @functools.lru_cache(maxsize=128)
 def get_stations():
-    engine = connect_to_database()
-    sql = "select * from station;"
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(text(sql)).fetchall()
-            print('#found {} stations', len(rows), rows)
-            return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
-    except:
+    try: 
+        # Retrieve the data passed into the function and load it as a JSON object
+        text = requests.get(URI1).text
+        stations = json.loads(text)
+        # Loop through each station in the JSON object and extract the necessary values
+        vals = []
+        for station in stations:
+            vals.append((station.get('number'), station.get('available_bikes'), station.get('available_bike_stands'), station.get('status'), datetime.timestamp(datetime.now())))
+        print('#found {} Availability {}'.format(len(vals), vals))
+            
+        # Print out the number of stations found and the extracted values
+        print('#found {} stations {}'.format(len(vals), vals))
+        return stations
+    except Exception as e:
         print(traceback.format_exc())
-        return "error in get_stations", 404
+        return "Error in get_stations: " + str(e), 404
 
-@app.route("/occupancy/<int:number>")
-def get_occupancy(number):
-    engine = connect_to_database()
-    sql = f"""SELECT address, a.available_bike_stands as bike_stands,
-    a.available_bikes as bikes_free
-    FROM dbbikes2.availability2 as a
-    JOIN dbbikes2.station as s
-    ON s.number = a.number
-    WHERE s.number = {number}
-    GROUP BY s.address
-    ORDER BY s.address;"""
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(text(sql)).fetchall()
-            print('#found {} stations', len(rows), rows)
-            return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
-    except:
-        print(traceback.format_exc())
-        return "error in get_stations", 404
- 
+##URL's with parameters already included
+URI1 =  "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
 
 
 if __name__ == "__main__":
+    # Start a new thread to continuously update the data
+    import threading
+    t = threading.Thread(target=update_data)
+    t.start()
     app.run(debug=True)
 
 
