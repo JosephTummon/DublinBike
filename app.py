fb6d2ecf0be83244663bb9687e89ebe2677dcd90
bomahony1 <bomahony1999@gmail.com>
2023-04-05 12:46:36 +0100

    Fixed server bug

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ PORT = "3306"
 DB = "dbbikes2"
 USER = "admin"
 PASSWORD = "DublinBikes1"
-engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+# engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
 # Initialize Flask app
 app = Flask("__name__")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 ##JCDeaux API Key
 URI1 =  "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
@@ -41,6 +45,7 @@ def get_stations():
     except Exception as e:
         print(traceback.format_exc())
         return "Error in get_stations: " + str(e), 404
+    
 
 
 # Define the main function that will call the get_stations function every 30 seconds
@@ -50,7 +55,6 @@ def update_data():
             # Call the get_stations function and update the stations variable
             stations = get_stations()
             print("Data updated at {}".format(datetime.now()))
-
             # Sleep for 30 seconds before calling the function again
             time.sleep(30)
         except Exception as e:
@@ -58,6 +62,7 @@ def update_data():
             print("Error updating data: {}".format(e))
             # Sleep for 30 seconds before calling the function again
             time.sleep(30)
+    
 
 
 if __name__ == "__main__":
@@ -66,45 +71,3 @@ if __name__ == "__main__":
     t = threading.Thread(target=update_data)
     t.start()
     app.run(debug=True)
-
-
-# CODE FOR TAKING DATA FROM DATABASE - NEED TO CONFIRM WITH GUS THAT WE CAN JUST PULL DATA STRAIGHT FROM JCDEAUX API
-# # Connect to database
-# def connect_to_database():
-#     engine = create_engine("mysql://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB), echo=True)
-#     return engine
-
-
-# # Load stations from database
-# def load_stations_from_db():
-#     engine = connect_to_database()
-#     with engine.connect() as conn:
-#         result = conn.execute(text("SELECT * FROM station"))
-#         stations = []
-#         for row in result.all():
-#             stations.append((dict(row)))
-#         return stations
-
-# def load_station_coordinates():
-#     engine = connect_to_database()
-#     with engine.connect() as conn:
-#         result = conn.execute(text("SELECT position_lat, position_lng FROM dbbikes2.station;"))
-#         co_ordinates = []
-#         for row in result.all():
-#             co_ordinates.append((dict(row)))
-#         return co_ordinates
-
-# def load_availability_from_db():
-#     engine = connect_to_database()
-#     with engine.connect() as conn:
-#         result = conn.execute(text("SELECT * FROM availability2"))
-#         availabile_bikes = []
-#         for row in result.all():
-#             availabile_bikes.append((dict(row)))
-#         return availabile_bikes
-
-# @app.route('/')
-# def home():
-#     stations = load_stations_from_db()
-#     co_ordinates = load_station_coordinates()
-#     return render_template('index.html')
