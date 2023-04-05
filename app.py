4ccbed89b06d76559792a0f3bb052c14ee3303ca
bomahony1 <bomahony1999@gmail.com>
2023-04-05 14:57:13 +0100

    Update frontend-branch

--- app.py
+++ app.py
@@ -2,87 +2,87 @@ from flask import Flask, g, render_template, jsonify
 from sqlalchemy import create_engine, text
 import traceback
 import functools
+import requests
+import time
+import simplejson as json
+from datetime import datetime
 
+# Database configuration
 URI = "dbbikes2.cytgvbje9wgu.us-east-1.rds.amazonaws.com"
 PORT = "3306"
 DB = "dbbikes2"
 USER = "admin"
 PASSWORD = "DublinBikes1"
-engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+# engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-app = Flask("__name__", template_folder="templates")
-app._static_folder = "static "
-
-def connect_to_database():
-    engine = create_engine("mysql://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB), echo=True)
-    return engine
-
-  
-def load_stations_from_db():
-    engine = connect_to_database()
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM station"))
-        stations = []
-        for row in result.all():
-            stations.append((dict(row)))
-        return stations
-
-def load_station_coordinates():
-    engine = connect_to_database()
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT position_lat, position_lng FROM dbbikes2.station;"))
-        co_ordinates = []
-        for row in result.all():
-            co_ordinates.append((dict(row)))
-        return co_ordinates
-
-def load_availability_from_db():
-    engine = connect_to_database()
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM availability2"))
-        availabile_bikes = []
-        for row in result.all():
-            availabile_bikes.append((dict(row)))
-        return availabile_bikes
+# Initialize Flask app
+app = Flask("__name__")
 
 @app.route('/')
 def home():
-    stations = load_stations_from_db()
-    co_ordinates = load_station_coordinates()
     return render_template('index.html')
 
+##JCDeaux API Key
+JCDEAUXAPI =  "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=8ad0fc88de299d032d91bc99f1e01c34a44d39a0"
+
+# Define the function to get availability data
 @app.route("/stations")
 @functools.lru_cache(maxsize=128)
 def get_stations():
-    engine = connect_to_database()
-    sql = "select * from station;"
-    try:
-        with engine.connect() as conn:
-            rows = conn.execute(text(sql)).fetchall()
-            print('#found {} stations', len(rows), rows)
-            return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
-    except:
-        print(traceback.format_exc())
-        return "error in get_stations", 404
-
-# @app.route("/availability")
-# @functools.lru_cache(maxsize=128)
-# def get_availability():
-#     engine = connect_to_database()
-#     sql = "SELECT station.address, availability2.available_bikes FROM station JOIN available2 ON station.number = availability2.number;"
-#     try:
-#         with engine.connect() as conn:
-#             rows = conn.execute(text(sql)).fetchall()
-#             print('#found {} availability', len(rows), rows)
-#             return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
-#     except:
-#         print(traceback.format_exc())
-#         return "error in get_availability", 404
-
-
+    try: 
+        # Retrieve the data passed into the function and load it as a JSON object
+        text = requests.get(JCDEAUXAPI).text
+        stations = json.loads(text)
+        # Loop through each station in the JSON object and extract the necessary values
+        vals = []
+        for station in stations:
+            vals.append((station.get('number'), station.get('available_bikes'), station.get('available_bike_stands'), station.get('status'), datetime.timestamp(datetime.now())))
+        print('#found {} Availability {}'.format(len(vals), vals))
     
+        return stations
+    except Exception as e:
+        print(traceback.format_exc())
+        return "Error in get_stations: " + str(e), 404
+
+
+##Weather API Key
+WEATHERAPI = "http://api.openweathermap.org/data/2.5/weather?appid=d5de0b0a9c3cc6473da7d0005b3798ac&q=Dublin, IE"
+
+# Define the function to get weather data
+@app.route("/weather")
+def get_weather():
+    try: 
+        # Retrieve the data passed into the function and load it as a JSON object
+        text = requests.get(WEATHERAPI).text
+        weather = json.loads(text)
+        # Extract the necessary values from the JSON object
+        vals = (weather["weather"][0]["main"], weather["weather"][0]["description"], weather["main"]["temp"], weather["visibility"], weather["wind"]["speed"], weather["wind"]["deg"], weather["clouds"]["all"], datetime.timestamp(datetime.now()))
+        print('#found {} Availability {}'.format(len(vals), vals))
+        return weather
+    except Exception as e:
+        print(traceback.format_exc())
+        return "Error in get_weather: " + str(e), 404
+    
+# Define the main function that will call the get_stations function every 30 seconds
+def update_data():
+    while True:
+        try:
+            # Call the get_stations function and update the stations variable
+            stations = get_stations()
+            print("Data updated at {}".format(datetime.now()))
+            # Sleep for 30 seconds before calling the function again
+            time.sleep(30)
+        except Exception as e:
+            print(traceback.format_exc())
+            print("Error updating data: {}".format(e))
+            # Sleep for 30 seconds before calling the function again
+            time.sleep(30)
+        return stations
+
 
 if __name__ == "__main__":
+    # Start a new thread to continuously update the data
+    import threading
+    t = threading.Thread(target=update_data)
+    t.start()
     app.run(debug=True)
-
-
---
+++ static/index.js
@@ -0,0 +1,248 @@
+// Declare map and marker variables
+let map;
+let marker;
+
+// Initialize and add the map
+function initMap() {
+  // Set the coordinates for the center of the map
+  const dublin = { lat: 53.350140, lng: -6.266155 }
+
+  // Create a new map instance and set its center and zoom level
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: dublin,
+    zoom: 14,
+  });
+
+  // Fetch station data and display markers and drop-down options
+  fetchStationData();
+  // Fetch weather data 
+  fetchWeather();
+
+  // Update station data every 30 seconds
+  setInterval(fetchStationData, 30000);
+
+
+// fetch stations
+function fetchStationData() {
+  fetch("/stations") 
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('fetch response', typeof data);
+      displayDropDown(data);
+      addMarkers(data);
+    });
+}
+
+function fetchWeather() {
+  fetch("/weather")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('fetch response', typeof data);
+    displayWeather(data);
+  });
+}
+
+function displayWeather(data) {
+  console.log(data.weather.main);
+  var li = document.createElement(li);
+  li.innerHTML = data.weather.main;
+  document.getElementById("weather").appendChild(li);
+}
+
+// Display Dropdown in HTML
+function displayDropDown(stations) {
+  // Display drop down for start destination
+  stations.forEach(station=> {
+    var option = document.createElement("option");
+    option.setAttribute("id", "start-option");
+    option.value = station.address + ', Dublin';
+    option.innerHTML = station.address;
+    document.getElementById("start").appendChild(option);
+  })
+
+  // Display drop down for end destination
+  stations.forEach(station=> {
+    var option = document.createElement("option");
+    option.setAttribute("id", "end-option");
+    option.value = station.address + ", Dublin";
+    option.innerHTML = station.address;
+    document.getElementById("end").appendChild(option);
+  })
+}
+
+  
+  // ***** CODE FOR ADDING MARKERS AND INFO-WIDOW*****
+
+  // Create arrays to store the markers and info windows
+  const markerArray = [];
+  const infoWindowArray = [];
+
+  // Displays the station data on the map as markers and info windows
+function addMarkers(stations) {
+  
+  // Loop through each station and create a marker and info window for it
+  for (const station of stations) {
+    // Create a new marker for the station
+    var marker = createMarker(station);
+    markerArray.push(marker);
+
+    // Create a new info window for the marker
+    var infoWindow = createInfoWindow(station);
+    infoWindowArray.push(infoWindow);
+
+    // Attach listeners to show and hide the info window when the marker is hovered over
+    attachInfoWindowListeners(marker, infoWindow);
+  }
+}
+
+// Creates a new marker object for the given station and adds it to the map
+function createMarker(station) {
+  var myLatlng = { lat: station.position.lat, lng: station.position.lng };
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: station.address,
+    station_number: station.number,
+    bikes_free: station.available_bikes,
+    free_stands: station.available_bike_stands,
+  });  
+  marker.setLabel(station.available_bikes.toString());
+  return marker;
+}
+
+// Creates a new info window object for the given station
+function createInfoWindow(station) {
+  const contentString = `
+    <div class="info-window">
+      <h1>${station.address}</h1>
+      <h2>Available Bikes:</h2>
+      <p>${station.available_bikes}</p>
+      <h2>Available Stands:</h2>
+      <p>${station.available_bike_stands}</p>
+    </div>
+  `;
+  const infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
+  return infoWindow;
+}
+
+// Attaches listeners to show and hide the info window when the marker is hovered over
+function attachInfoWindowListeners(marker, infoWindow) {
+  let currentInfoWindow = null;
+  google.maps.event.addListener(marker, "mouseover", function() {
+    if (currentInfoWindow) {
+      currentInfoWindow.close();
+    }
+    infoWindow.open(map, marker);
+    currentInfoWindow = infoWindow;
+  });
+
+  google.maps.event.addListener(marker, 'mouseout', function() {
+    infoWindow.close();
+    currentInfoWindow = null;
+  });
+}
+     
+
+
+  // ***** CODE FOR DIRECTIONS *****
+
+   // Instantiate a directions service.
+   const directionsService = new google.maps.DirectionsService();
+   // Create a renderer for directions and bind it to the map.
+   const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+   // Instantiate an info window to hold step text.
+   const stepDisplay = new google.maps.InfoWindow();
+
+  // Display the route between the initial start and end selections.
+  calculateAndDisplayRoute(
+    directionsRenderer,
+    directionsService,
+    markerArray,
+    stepDisplay,
+    map
+  );
+
+  // Listen to change events from the start and end lists.
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(
+      directionsRenderer,
+      directionsService,
+      markerArray,
+      stepDisplay,
+      map
+    );
+  };
+
+  document.getElementById("start-option").addEventListener("change", onChangeHandler);
+  document.getElementById("end-option").addEventListener("change", onChangeHandler);
+  
+
+  function calculateAndDisplayRoute(
+  directionsRenderer,
+  directionsService,
+  markerArray,
+  stepDisplay,
+  map
+  ) {
+  // First, remove any existing markers from the map.
+  for (let i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(null);
+  }
+  
+  // Retrieve the start and end locations and create a DirectionsRequest using
+  // WALKING directions.
+  directionsService
+    .route({
+      origin: document.getElementById("start-option").value,
+      destination: document.getElementById("end-option").value,
+      travelMode: google.maps.TravelMode.BICYCLING,
+    })
+    .then((result) => {
+      // Route the directions and pass the response to a function to create
+      // markers for each step.
+      document.getElementById("warnings-panel").innerHTML =
+        "<b>" + result.routes[0].warnings + "</b>";
+      directionsRenderer.setDirections(result);
+      showSteps(result, markerArray, stepDisplay, map);
+    })
+    .catch((e) => {
+      window.alert("Directions request failed due to " + e);
+    });
+  }
+
+  function showSteps(directionResult, markerArray, stepDisplay, map) {
+  // For each step, place a marker, and add the text to the marker's infowindow.
+  // Also attach the marker to an array so we can keep track of it and remove it
+  // when calculating new routes.
+  const myRoute = directionResult.routes[0].legs[0];
+
+  for (let i = 0; i < myRoute.steps.length; i++) {
+    const marker = (markerArray[i] =
+      markerArray[i] || new google.maps.Marker());
+
+    marker.setMap(map);
+    marker.setPosition(myRoute.steps[i].start_location);
+    attachInstructionText(
+      stepDisplay,
+      marker,
+      myRoute.steps[i].instructions,
+      map
+    );
+  }
+  }
+
+  function attachInstructionText(stepDisplay, marker, text, map) {
+  google.maps.event.addListener(marker, "click", () => {
+    // Open an info window when the marker is clicked on, containing the text
+    // of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+  });
+
+}
+}
+
+window.initMap = initMap;
